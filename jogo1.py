54fc7c1cbbe63d73375bcbe5418afb772a9e0acb
Joao Ferraz <joaoaf@al.insper.edu.br>
2022-05-17 14:31:48 -0300

    Tentativa 1

--- jogo1.py
+++ jogo1.py
@@ -1,28 +1,58 @@
 #import bibliotecas
 import pygame
 import random
-import time
+
 
 pygame.init()
 
 #tela principal
 WIDTH = 480
 HEIGHT = 600
+FPS = 40
+tempo = pygame.time.Clock()
+
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Jogo do Dino')
 personagem_width = 40
 personagem_height = 35
 font = pygame.font.SysFont(None, 48)
 background = pygame.image.load('projeto-final-joao-gui-ricardo/fundo.png').convert()
 personagem_img = pygame.image.load('projeto-final-joao-gui-ricardo/dino.png').convert_alpha()
-meteor_img = pygame.transform.scale(personagem_img, (personagem_width, personagem_height))
-
+dino_img = pygame.transform.scale(personagem_img, (personagem_width, personagem_height))
 
 
 #classes
+class Dino(pygame.sprite.Sprite):
+    def _init_(self, img):
+        pygame.sprite.Sprite._init_(self)
+        self.image = img
+        self.image = pygame.Surface(50, 50)
+        self.image.fill(personagem_img)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 3, HEIGHT / 3)
+        self.speedx = 0
 
-#sprite for player
-
-
-#background
+    def update(self):
+        self.rect.x += self.speedx
 
 #repetir
+game = True
+#ajuste de velocidade
+tempo = pygame.time.clock()
+player = Dino(dino_img)
+
+all_sprites = pygame.sprite.Group()
+player = personagem_img()
+all_sprites.add(player)
+
+while game:
+    tempo.tick(FPS)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT():
+            game = False
+
+    #Update
+    all_sprites.uptade()
+
+
+pygame.quit()
