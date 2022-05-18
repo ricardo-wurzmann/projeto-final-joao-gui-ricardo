c02e7a0d7699241aebb65d1806b1d1dc0ce19a2e
guilhermechicharo <guilhermecff@al.insper.edu.br>
2022-05-18 08:05:53 -0300

    Continuação Tela Inicial

--- telainicial.py
+++ telainicial.py
@@ -11,33 +11,41 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Jogo do Dinossauro')
 
 # ----- Inicia estruturas de dados
-game = True
+running = True
 
 # ----- Inicia assets
-image = pygame.image.load('projeto-final-joao-gui-ricardo/fundo1.png').convert()
-image = pygame.transform.scale(image, (WIDTH, HEIGHT))
+def telainical(tela):
+    clock = pygame.time.Clock()
+    image = pygame.image.load('projeto-final-joao-gui-ricardo/fundo1.png').convert()
+    image = pygame.transform.scale(image, (WIDTH, HEIGHT))
 
-inicial = True
+    inicial = True
 
-font = pygame.font.SysFont(None, 48)
-text = font.render( 'Aperte Qualquer Tecla para Jogar', True, (255, 255, 255))
-window.blit(text, (20, 100))
-image.blit(text,(20,100))
+    font = pygame.font.SysFont(None, 48)
+    text = font.render( 'Aperte Qualquer Tecla para Jogar', True, (255, 255, 255))
+    window.blit(text, (20, 100))
+    image.blit(text,(20,100))
 
-# ===== Loop principal =====
-while inicial:
-    # ----- Trata eventos
-    for event in pygame.event.get():
-        # ----- Verifica consequências
-        if event.type == pygame.KEYUP:
-            inicial = False
+    # ===== Loop principal =====
+    while inicial:
+        clock.tick(45)
+        # ----- Trata eventos
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                state = QUIT
+                inicial = False
+            if event.type == pygame.KEYUP:
+                state = GAME
+                inicial = False
 
-    # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(image, (10, 10))
 
-    # ----- Atualiza estado do jogo
-    pygame.display.update()  # Mostra o novo frame para o jogador
+        # ----- Gera saídas
+        window.fill((0, 0, 0))  # Preenche com a cor branca
+        window.blit(image, (10, 10))
+
+        # ----- Atualiza estado do jogo
+        pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  
