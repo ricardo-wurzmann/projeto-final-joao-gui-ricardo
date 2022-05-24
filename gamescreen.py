fc9401a7365fd67fed03d48caa59eb29fab4b0df
guilhermechicharo <guilhermecff@al.insper.edu.br>
2022-05-24 10:35:39 -0300

    jogo finla

--- gamescreen.py
+++ gamescreen.py
@@ -95,3 +95,14 @@ telainicial.telainical(screen)
 
 
 pygame.quit()
+
+
+'''while GAME:
+        tempo.tick(FPS)
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT():
+                GAME = False
+            
+            if i.type == pygame.KEYDOWN:
+                if i.key == pygame.K_SPACE or i.key == pygame.K_UP:
+                    Dino.pulo()'''
