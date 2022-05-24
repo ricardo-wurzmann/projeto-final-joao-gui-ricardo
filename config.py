06121e7e6dd130cd0d860265879f90858b60e7a5
guilhermechicharo <guilhermecff@al.insper.edu.br>
2022-05-24 10:35:54 -0300

    jogo final

--- config.py
+++ config.py
@@ -31,7 +31,8 @@ YELLOW = (255, 255, 0)
 # Estados para controle do fluxo da aplicação
 INIT = 0
 GAME = 1
-QUIT = 2
+END = 2
+QUIT = 3
 
 #gravidade 
 GRAVIDADE = 2
--- jogo_final.py
+++ jogo_final.py
@@ -1,9 +1,31 @@
-'''while GAME:
-        tempo.tick(FPS)
-        for i in pygame.event.get():
-            if i.type == pygame.QUIT():
-                GAME = False
-            
-            if i.type == pygame.KEYDOWN:
-                if i.key == pygame.K_SPACE or i.key == pygame.K_UP:
-                    Dino.pulo()'''
+
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT, END
+from tela_final import telafinal
+from gamescreen import gamescreen
+from telainicial import telainical
+
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = telainical(window)
+    elif state == GAME:
+        state = gamescreen(window)
+    elif state == END:
+        state = telafinal(window)
+    else:
+        state = QUIT
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
