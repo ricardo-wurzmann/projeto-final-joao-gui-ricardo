f3c2462878467baba44c77326aabc5b1e2c37303
ricardo-wurzmann <ricardow3@al.insper.edu.br>
2022-05-25 10:43:49 -0300

    assets

--- assets.py
+++ assets.py
@@ -4,10 +4,17 @@ from config import *
 
 
 def load_assets():
+
+    background = pygame.image.load(path.join(IMG_DIR,'fundo1.png')).convert()
+    background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+    personagem_img = pygame.image.load(path.join(IMG_DIR,'dino.png')).convert_alpha()
+    dino_img = pygame.transform.scale(personagem_img, (personagem_width, personagem_height))
+    cacto_img = pygame.image.load(path.join(IMG_DIR,'Cacto.png')).convert_alpha() 
+    cacto_img = pygame.transform.scale(cacto_img, (cacto_width, cacto_height))
+
     assets = {}
     assets[background] = pygame.image.load(os.path.join(IMG_DIR, 'fundo1.png')).convert()
     assets[dino_img] = pygame.image.load(os.path.join(IMG_DIR, 'dino.png')).convert_alpha()
-    assets[dino_img] = pygame.transform.scale(assets['ship_img'], (personagem_width, personagem_height))
     assets[cacto_img] = pygame.image.load(os.path.join(IMG_DIR, 'Cacto.png')).convert_alpha()
 
     # Carrega os sons do jogo
