d7171f8a329d48839c13b96586b9089b598f51b4
joaoaf1 <joaoaf@al.insper.edu.br>
2022-05-26 14:44:03 -0300

    Troca de imagens

--- assets.py
+++ assets.py
@@ -8,8 +8,8 @@ def load_assets(IMG_DIR):
     assets = {}
     assets['dino'] = pygame.image.load(path.join(IMG_DIR, 'dino.png')).convert_alpha()
     assets['dino'] = pygame.transform.scale(assets['dino'], (80,100))
-    assets['cacto'] = pygame.image.load(path.join(IMG_DIR, 'Cacto.png')).convert()
-    assets['cacto'] = pygame.transform.scale(assets['cacto'], (80,tam_cact))
+    assets['cacto'] = pygame.image.load(path.join(IMG_DIR, 'Cacto2.png')).convert_alpha()
+    assets['cacto'] = pygame.transform.scale(assets['cacto'], (70,tam_cact))
     assets['fundo'] = pygame.image.load(path.join(IMG_DIR, 'fundo1.png')).convert()
     assets['fundo'] = pygame.transform.scale(assets['fundo'], (2800,1400))
     
